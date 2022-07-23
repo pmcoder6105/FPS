c431657fe8cd97e852fd1c3b19247d6cbf67bc81
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-07-23 16:30:26 -0700

    Made a weapon + stats viewer
    Looks good. Also hooked up quit button

---
+++ FPS-Game/Assets/WeaponViewerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponViewerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDrag()
+    {
+        float rotSpeed = 100;
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX);
+        transform.Rotate(Vector3.right, -rotY);
+    }
+
+}
