ee687a7770bd5b0bf3778504904f8b2288cf5762
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-12-24 18:26:08 -0800

    Added bad word hiding
    I really didn't want to type those things in...

---
+++ FPS-Game/Assets/MyScripts/BadWordManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BadWordManager : MonoBehaviour
+{
+    InputField inFieldText;
+    string myString;
+    [SerializeField] string[] badWords = { "fuck", "shit", "cock", "tit", "pussy", "asshole", "cunt", "nigg"};
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inFieldText = GetComponent<InputField>();
+    }
+
+    public void ChangeName(string nameIn)
+    {
+        myString = nameIn;
+        BadWordParser();
+    }
+
+    void BadWordParser()
+    {
+        for (int i = 0; i < badWords.Length; i++)
+        {
+            if (myString.ToLower().Contains(badWords[i]))
+            {
+                for (int j = 0; j < myString.Length; j++)
+                {
+                    if (myString.ToLower()[j] == badWords[i][0])
+                    {
+                        string temp = myString.Substring(j, badWords[i].Length);
+                        if (temp.ToLower() ==  badWords[i])
+                        {
+                            myString = myString.Remove(j, badWords[i].Length);
+                            if (myString != null)
+                            {
+                                inFieldText.text = myString.ToString();
+                            }
+                            else
+                            {
+                                inFieldText.text = "";
+                            }
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
