9341dada1d5e3eae25219915d228033f3ba53822
Pratyush <pratyush.mahadevaiah@gmail.com>
2023-01-20 17:41:37 -0800

    Started working on the new parkour game mode

---
+++ FPS-Game/Assets/GrappleGun.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleGun : MonoBehaviour
+{
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappleable;
+    public Transform tip;
+    public Transform cam;
+    public Transform player;
+    private float maxDistance = 100f;
+    private SpringJoint joint;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }
+    }
+
+    void StartGrapple()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxDistance, whatIsGrappleable))
+        {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+            joint.maxDistance = distanceFromPoint * 0.8f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+        }
+    }
+
+    void StopGrapple()
+    {
+
+    }
+}
