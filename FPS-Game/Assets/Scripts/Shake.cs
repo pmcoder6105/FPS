e4a52c56e8a14112c0c1f49cd383f22044d05e83
PMGames <pratyush.mahadevaiah@gmail.com>
2023-02-09 22:11:15 -0800

    Did changes necessary for Windows i7

--- FPS-Game/Assets/Scripts/Shake.cs
+++ FPS-Game/Assets/Scripts/Shake.cs
@@ -8,12 +8,6 @@ public class Shake : MonoBehaviour
     public float duration = 1f;
     public AnimationCurve curve;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
